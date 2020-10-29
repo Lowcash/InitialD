843fb7e07eb8ca5ea5b3979b5a6f5c88e5e0858c
Lowcash <lukas.lowcash@gmail.com>
2020-10-29 17:36:11 +0100

    Initial commit

---
+++ src/scripts/game.ts
@@ -0,0 +1,28 @@
+import 'phaser'
+import MainScene from './scenes/mainScene'
+import PreloadScene from './scenes/preloadScene'
+
+const DEFAULT_WIDTH = 1280
+const DEFAULT_HEIGHT = 800
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  backgroundColor: '#ffffff',
+  scale: {
+    parent: 'initial-d-game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT
+  },
+  scene: [PreloadScene, MainScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: /*300*/ 0 }
+    }
+  }
+}
+
+export default new Phaser.Game(config)
